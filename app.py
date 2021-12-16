5cdd365a91f3eb068ab5ff4fcc01c19c7e116be2
CBlumini <72533588+CBlumini@users.noreply.github.com>
2021-12-15 18:17:22 -0800

    Able to pull a CSV from github for data

--- app.py
+++ app.py
@@ -14,38 +14,16 @@ from dash.dependencies import Input, Output
 import dash_table
 import os
 import time
-#import boto3
 
-AWS_S3_BUCKET = os.getenv("AWS_S3_BUCKET")
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
-
-
-#s3 = boto3.client('s3')
-#obj = s3.get_object(Bucket='tridata', Key='key')
-
-#print('hello')
-
-#this would be the point to read the data from the DB
-
-#can just provide an s3 or github link 
-#data = pd.read_excel('santa_cruz_data.xlsx', header = 0, index_col=None)
-# data = pd.read_excel(f"s3://tridata/santa_cruz_data.xlsx",
-#                     storage_options = {
-#                                 "key": AWS_ACCESS_KEY_ID,
-#                                  "secret": AWS_SECRET_ACCESS_KEY,
-#                                  "token": AWS_SESSION_TOKEN,
-#                     }, header = 0, index_col=None)
-# #data.head()
-data = pd.read_excel('https://github.com/CBlumini/heroku_dep_2/blob/9392294f52b096a61d131018eb317ba81efc798e/santa_cruz_data.xlsx?raw=true', header=0, index_col=None)
+data = pd.read_csv('https://github.com/CBlumini/heroku_dep_2/raw/main/Santa-Cruz-Sprint.csv', header=0, index_col=None)
 
 #the data does not come in the right form to do math on it. So convert the times to minutes and decimal seconds
 #maybe setup a compute file to do this by itself later
 def create_time_columns(bare_frame):
     def convertTime (time):
-        timeMinutes = (time.hour*60)+(time.minute)+(time.second/60)
-        return round(timeMinutes, 2)
+        temp = time.split(':')
+        timeMinutes = (int(temp[0])*60)+int(temp[1])+int(temp[2])/60
+        return timeMinutes
 
     #convert to integers
     bare_frame["Swim Minutes"] = bare_frame["Swim"].apply(convertTime)
